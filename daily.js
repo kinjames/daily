1e0142ed13d8ff7a6b36af9726543a065d6a38fd
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-03-26 22:34:22 +0000

    Add files via upload

---
+++ daily.js
@@ -0,0 +1,207 @@
+const localQuotes = [
+    {
+      "text": "God comforts me in all tribulations.",
+      "author": "2Cor 1:3-4"
+    },
+    {
+      "text": "I am sufficient in God.",
+      "author": "2Cor 3:5"
+    },
+    {
+      "text": "I will fear no evil.",
+      "author": "Psalm 23:4"
+    },
+    {
+      "text": "My voice is heard by God.",
+      "author": "Psalm 116:1-2"
+    },
+    {
+      "text": "God is my helper",
+      "author": "Psalm 121:1-2"
+    },
+    {
+      "text": "I will not be moved.",
+      "author": "Psalm 125:1"
+    },
+    {
+      "text": "God makes me strong.",
+      "author": "Psalm 138:3"
+    },
+    {
+      "text": "God rescues me from trouble.",
+      "author": "Psalm 138:7"
+    },
+    {
+      "text": "God heals my wounds.",
+      "author": "Psalm 147:3"
+    },
+    {
+      "text": "My soul finds joy in the Lord.",
+      "author": "Psalm 35:9"
+    },
+    {
+      "text": "God will make my dreams come true.",
+      "author": "Psalm 37:4"
+    },
+    {
+      "text": "God is my strength.",
+      "author": "Psalm 37:39"
+    },
+    {
+      "text": "My steps are established by God.",
+      "author": "Psalm 40:2"
+    },
+    {
+      "text": "God is always present in my life.",
+      "author": "Psalm 46:1-2"
+    },
+    {
+      "text": "Be still my heart and know that He is God.",
+      "author": "Psalm 46:10"
+    },
+    {
+      "text": "God will guide me forever.",
+      "author": "Psalm 48:14"
+    },
+    {
+      "text": "I will trust in God.",
+      "author": "Psalm 56:3-4"
+    },
+    {
+      "text": "My soul only waits in God.",
+      "author": "Psalm 62:5-7"
+    },
+    {
+      "text": "I am blessed with every spiritual blessing.",
+      "author": "Ephesians 1:3"
+    },
+    {
+      "text": "I am forgiven.",
+      "author": "Ephesians 1:7"
+    },
+    {
+      "text": "My heart and mind will have peace.",
+      "author": "Philippians 4:7"
+    },
+    {
+      "text": "I am chosen by God.",
+      "author": "Ephesians 1:4"
+    },
+    {
+      "text": "I am complete in God.",
+      "author": "Colossians 2:10"
+    },
+    {
+      "text": "I find rest in God.",
+      "author": "Hebrews 4:9"
+    },
+    {
+      "text": "When i am weak, I am strong.",
+      "author": "2 Cor 12:10"
+    },
+    {
+      "text": "God wants me to prosper in all things.",
+      "author": "3 John 1:2"
+    },
+    {
+      "text": "I trust in God's timing.",
+      "author": "Ecclesiastes 3:1"
+    },
+    {
+      "text": "I can see beauty in everything.",
+      "author": "Ecclesiastes 3:11"
+    },
+    {
+      "text": "I will enjoy the good in my life",
+      "author": "Ecclesiastes 3:12-13"
+    },
+    {
+      "text": "I am loved.",
+      "author": "Isaiah 43:4"
+    },
+    {
+      "text": "I take comfort in belonging to God.",
+      "author": "Isaiah 43:1"
+    },
+    {
+      "text": "My past does not define me.",
+      "author": "Isaiah 43:18-19"
+    },
+    {
+      "text": "I will not be ashamed.",
+      "author": "Isaiah 54:4"
+    },
+    {
+      "text": "God's mercy won't abandon me.",
+      "author": "Isaiah 54:10"
+    },
+    {
+      "text": "I will be guided continually by the Lord.",
+      "author": "Isaiah 58: 11"
+    },
+    {
+      "text": "God is my eternal light.",
+      "author": "Isaiah 60:19-20"
+    },
+    {
+      "text": "God will meet all my needs according to the riches of his glory in Christ Jesus.",
+      "author": "Philippians 4:19"
+    },
+    {
+      "text": "God answers my prayers.",
+      "author": "Mark 11:24"
+    },
+    {
+      "text": "My heart is happy.",
+      "author": "Psalm 16:9"
+    },
+    {
+      "text": "God is my healer.",
+      "author": "Jeremiah 17:14"
+    },
+    {
+      "text": "I have found life in God.",
+      "author": "Proverbs 8:35"
+    },
+    {
+      "text": "God comforts my anxiety.",
+      "author": "Psalm 94:19"
+    },
+    {
+      "text": "God has great plans for me.",
+      "author": "Jeremiah 29:11"
+    },
+    {
+      "text": "I am blessed.",
+      "author": "Psalm 84:4"
+    },
+    {
+      "text": "God always saves me.",
+      "author": "Psalm 55:16"
+    },
+    {
+      "text": "I will walk by faith and not by sight.",
+      "author": "2 Cor 5:7"
+    },
+    {
+      "text": "God will keep me in perfect peace.",
+      "author": "Isaiah 26:3"
+    },
+    {
+      "text": "Nothing is impossible for me.",
+      "author": "Matthew 17:20"
+    },
+    {
+      "text": "I will not give up.",
+      "author": "Galatians 6:9"
+    },
+    {
+      "text": "I am strong and courageous.",
+      "author": "Joshua 1:9"
+    },
+    {
+      "text": "I find joy in God's mercy.",
+      "author": "Psalm 31:7"
+    }
+];
+  
